c28d4b8b3facfc1abc49930864b8759810a37fd6
TranHa9 <tranha.99884@gmail.com>
2024-08-26 21:16:47 +0700

    update

---
+++ JS-01/js/demo01.js
@@ -0,0 +1,11 @@
+function changeH1Text() {
+    //innerHTML thay đổi text
+    document.getElementsByTagName('h1')[0].innerHTML = 'Text mới';
+}
+function addNewTag() {
+    //write: viết ra một trang mới
+    document.write("Nội dung mới");
+}
+function printConsole() {
+    console.log(5 + 6)
+}
