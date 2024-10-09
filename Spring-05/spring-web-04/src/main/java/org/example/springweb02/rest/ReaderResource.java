b61e79f6edf0658131bceab7c2d7ed5ff0461baf
TranHa9 <tranha.99884@gmail.com>
2024-10-09 11:46:16 +0700

    update

---
+++ Spring-05/spring-web-04/src/main/java/org/example/springweb02/rest/ReaderResource.java
@@ -0,0 +1,42 @@
+package org.example.springweb02.rest;
+
+import org.example.springweb02.model.request.ReaderCreationRequest;
+import org.example.springweb02.model.request.ReaderUpdateRequest;
+import org.example.springweb02.model.response.ReaderResponse;
+import org.example.springweb02.service.ReaderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/readers")
+public class ReaderResource {
+    @Autowired
+    ReaderService readerService;
+
+    @GetMapping
+    public List<ReaderResponse> getAll(){
+        return readerService.getAll();
+    }
+
+    @PostMapping
+    public ReaderResponse createReader(@RequestBody ReaderCreationRequest request){
+        return readerService.create(request);
+    }
+
+    @GetMapping("/{id}")
+    public ReaderResponse getDetail(@PathVariable int id){
+        return readerService.getDetail(id);
+    }
+
+    @PutMapping("/{id}")
+    public ReaderResponse updateReader(@PathVariable int id, @RequestBody ReaderUpdateRequest request){
+        return readerService.update(id, request);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteReader(@PathVariable int id){
+        readerService.deleteReader(id);
+    }
+}
